6086d01575ddc9be06c8c584a87111ed011c412b
fibll <acc.simonretsch@icloud.com>
2018-01-10 17:31:24 +0100

    Remove last update with exapmle project

--- source.js
+++ source.js
@@ -1,6 +1,18 @@
 var vue = new Vue({
     el: "#tmp",
     data: {
-        name: "bla"
+        name: "bla",
+        jsonData: {system:[] , process:[]},
+    },
+
+    // get json stuff
+    mounted(){
+        var itself = this;
+        $.getJSON("../src/process.json", function(data) {
+            itself.jsonData = data;
+            itself.json_tmp  = self.json_data.process.childs;
+            itself.child_num = JsonCount(self.json_data.process.childs);
+            //alert(JsonCount(self.json_data.process.childs));
+        });
     }
 })
