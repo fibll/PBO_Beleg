449a59c602f85f22408f3dc44d32a38b3f6ab9b7
fibll <acc.simonretsch@icloud.com>
2018-01-10 16:43:47 +0100

    Update with sample_project

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="container">
+    <div class="row">
+      <div class="col-xs-12">
+      </div>
+    </div>
+  </div>
+
+</template>
+
+<script>
+export default {
+
+}
+</script>
